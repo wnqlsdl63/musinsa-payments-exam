bf426b8c34cfbd8d0ac497099ad74281ccaf7c6d
ImJuBin <>
2024-11-06 00:50:55 +0900

    Fix: Exception Handler 예외처리 클래스 수정

--- src/main/java/com/musinsa_payments/exam/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/musinsa_payments/exam/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.musinsa_payments.exam.common.exception;
 
 import com.musinsa_payments.exam.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import lombok.extern.slf4j.Slf4j;
@@ -10,16 +12,26 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ApiResponse<String>> handleValidationException(ValidationException ex) {
-        log.error("Validation error: {}", ex.getMessage());
+    /**
+     * @Valid 어노테이션으로 유효성 검사를 통과하지 못한 예외를 처리합니다.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldErrors().getFirst();
+        String errorMessage = fieldError.getDefaultMessage();
 
-        return ApiResponse.exception(ex.getMessage());
+        log.error("Validation error: {}", errorMessage);
+        return ApiResponse.exception(errorMessage);
     }
 
+    /**
+     * 어플리케이션에서 잡지 못한 예외를 처리합니다.
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(Exception ex) {
         log.error("Internal server error: {}", ex.getMessage());
         return ApiResponse.error();
     }
+
+
 }
