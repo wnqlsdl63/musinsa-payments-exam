2646c8a9a7a684ce7b5943eb2f430a0d59e312ed
ImJuBin <>
2024-11-07 00:05:19 +0900

    Fix: 사용자의 보유 포인트 총합 조회 쿼리에 만료일 조건 추가

--- src/main/java/com/musinsa_payments/exam/domain/point/repository/PointRepository.java
+++ src/main/java/com/musinsa_payments/exam/domain/point/repository/PointRepository.java
@@ -8,7 +8,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
-    @Query("SELECT COALESCE(SUM(p.point), 0) FROM Point p WHERE p.userId = :userId")
-    int getCurrentPoints(@Param("userId") Long userId);
+    @Query("""
+                SELECT COALESCE(SUM(p.point), 0)
+                FROM Point p
+                WHERE p.userId = :userId
+                AND (p.expireDate IS NULL OR p.expireDate > CURRENT_TIMESTAMP)
+            """)
+    int getCurrentPointsByUserId(@Param("userId") Long userId);
 
 }
--- src/main/java/com/musinsa_payments/exam/domain/point/service/validator/PointValidator.java
+++ src/main/java/com/musinsa_payments/exam/domain/point/service/validator/PointValidator.java
@@ -1,23 +1,36 @@
 package com.musinsa_payments.exam.domain.point.service.validator;
 
-import com.musinsa_payments.exam.common.constant.PointPolicyConstant;
 import com.musinsa_payments.exam.common.exception.ValidationException;
 import com.musinsa_payments.exam.domain.point.repository.PointRepository;
 import com.musinsa_payments.exam.domain.policy.entity.UserPointPolicy;
 import com.musinsa_payments.exam.domain.policy.repository.UserPointPolicyRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import static com.musinsa_payments.exam.common.constant.PointPolicyConstant.DEFAULT_USER_MAX_POINT;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PointValidator {
-
     private final UserPointPolicyRepository userPointPoliciesRepository;
     private final PointRepository pointsRepository;
 
+
+    /**
+     * 사용자의 포인트 적립 가능 여부를 검증합니다.
+     *
+     * - 해당 사용자의 최대 포인트 제한을 조회합니다. 사용자별 제한이 설정되어 있지 않으면 기본 포인트 제한값(20만)을 사용합니다.
+     * - 현재 사용자가 보유하고 있는 포인트 총합에 적립하려는 금액을 더한 값이 최대 포인트 제한을 초과하는지 확인합니다.
+     * - 만약 초과할 경우, ValidationException을 발생시켜 적립을 제한합니다.
+     *
+     * @param userId 포인트를 적립하려는 사용자 ID
+     * @param amount 적립하려는 포인트 금액
+     * @throws ValidationException 최대 보유 가능 포인트 한도를 초과하는 경우 예외 발생
+     */
     public void validateAccumulatePoints(Long userId, int amount) {
+        log.debug("[validateAccumulatePoints] userId: {}, amount: {}", userId, amount);
         // 사용자 최대 포인트 제한 조회
         Integer maxPointsLimit = userPointPoliciesRepository
                 .findByUserId(userId)
@@ -25,8 +38,10 @@ public class PointValidator {
                 .orElse(DEFAULT_USER_MAX_POINT);
 
         // 현재 사용자 포인트 총합 가져와서 제한 확인
-        int currentPoints = pointsRepository.getCurrentPoints(userId);
-        int newTotalPoints = currentPoints + amount;
+        int currentPoint = pointsRepository.getCurrentPointsByUserId(userId);
+        log.debug("[validateAccumulatePoints] currentPoint: {}, maxPointsLimit: {}", currentPoint, maxPointsLimit);
+
+        int newTotalPoints = currentPoint + amount;
 
         if (newTotalPoints > maxPointsLimit) {
             throw new ValidationException("최대 보유 가능한 포인트 한도를 초과할 수 없습니다.");
